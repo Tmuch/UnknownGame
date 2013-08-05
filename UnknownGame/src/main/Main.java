ff5d867357185d1a49195051f9c997fca2afc8a7
Tyler Much <tyler.much@marquette.edu>
2013-08-04 19:20:05 -0500

    update

--- UnknownGame/src/main/Main.java
+++ UnknownGame/src/main/Main.java
@@ -1,10 +1,61 @@
 package main;
 
+import render.Window;
+
 public class Main
 {
+
+	public static final int		WIDTH	= 800;
+	public static final int		HEIGHT	= 600;
+	public static final String	TITLE	= "GAME";
+
+	private boolean				isRunning;
+
 	public static void main(String[] args)
 	{
+		Window.createWindow(WIDTH, HEIGHT, TITLE);
 
+		Main game = new Main();
+		game.start();
+	}
+
+	public Main()
+	{
+		isRunning = false;
+	}
+
+	public void start()
+	{
+		if (isRunning) return;
+		run();
+	}
+
+	public void stop()
+	{
+		if (!isRunning) return;
+		isRunning = false;
+	}
+
+	private void run()
+	{
+		isRunning = true;
+		while (isRunning)
+		{
+			if (Window.isCloseRequested()) stop();
+			render();
+		}
+
+		cleanup();
+	}
+
+	private void render()
+	{
+		Window.render();
+	}
+
+	private void cleanup()
+	{
+		Window.dispose();
 	}
 
 }
